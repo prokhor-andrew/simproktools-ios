51e2437b62c087e5250f736f9ecb44d2500f6036
prokhor-andrew <prokhor.andrew@gmail.com>
2023-02-18 19:14:28 -0500

    moved in converters from simprokstate

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/simprok-dev/simprokstate-ios.git",
-            exact: .init(1, 2, 16)
+            exact: .init(1, 2, 17)
         ),
     ],
     targets: [
