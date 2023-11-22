7882bbe56c75dfab2a75c58807ed02611906bd6a
prokhor-andrew <prokhor.andrew@gmail.com>
2023-11-22 15:45:52 -0600

    updated version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/simprok-dev/simprokstate-ios.git",
-            exact: .init(1, 2, 42)
+            exact: .init(1, 2, 43)
         ),
     ],
     targets: [
